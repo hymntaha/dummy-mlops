5f814457d59220f1d77bfad61ecf1663f3e72c2a
hymntaha <tahauygun@gmail.com>
2024-06-29 18:14:25 +0000

    setting up constructors

--- template.py
+++ template.py
@@ -5,5 +5,12 @@ project_name = "dummy_mlops"
 
 list_files = [
     f"{project_name}/__init__.py",
-    f"{project_name}/components/",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/components/data_ingestion.py",
+    f"{project_name}/components/data_validation.py",
+    f"{project_name}/components/data_transformation.py",
+    f"{project_name}/components/model_training.py",
+    f"{project_name}/components/model_evaluation.py",
+    f"{project_name}/components/model_pusher.py",
+    f"{project_name}/configuration/__init__.py",
 ]
