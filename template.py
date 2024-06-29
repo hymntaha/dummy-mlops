377aab87ece1787f113644d0ef1ff82672f8ee5a
hymntaha <tahauygun@gmail.com>
2024-06-29 18:14:46 +0000

    setting up constructors

--- template.py
+++ template.py
@@ -13,4 +13,5 @@ list_files = [
     f"{project_name}/components/model_evaluation.py",
     f"{project_name}/components/model_pusher.py",
     f"{project_name}/configuration/__init__.py",
+    f"{project_name}/constants/__init__.py",
 ]
