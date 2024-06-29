09d8c304a14d074bdc249eb0a37841049d8d3f93
hymntaha <tahauygun@gmail.com>
2024-06-29 18:25:11 +0000

    template setup

--- template.py
+++ template.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 
-project_name = "dummy_mlops"
+project_name = "us_visa"
 
 list_files = [
     f"{project_name}/__init__.py",
@@ -33,3 +33,14 @@ list_files = [
     "config/model.yaml",
     "config/schema.yaml"
 ]
+
+for filepath in list_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, 'w') as f:
+            pass
+    else:
+        printf(f"file exists in {filepath}")
